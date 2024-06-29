6970cb0f21c87bd8f0f8b725a2b66f4e4e70b928
Matias Caceres Dioverti <131722551+Matiasjcd@users.noreply.github.com>
2024-06-29 09:50:24 -0400

    Rename 11.sql to movies/11.sql

---
+++ movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies, ratings WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')) AND id = movie_id ORDER BY rating DESC LIMIT 5;
